6645958463c6fbd84b09c31b18e8022a33856d2e
Mounika <mounika@archeplay.com>
2020-11-20 13:10:42 +0000

    packer update

---
+++ script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set +x
+sudo yum install docker jq -y
+sudo usermod -a -G docker ec2-user
+sudo systemctl  start docker
+sudo systemctl status docker
+sudo systemctl enable docker
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo yum -y install git
+sudo git clone https://github.com/mohanraz81/archeplay-ecomm-demo.git
+REGION=`curl --silent http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region`
+sudo sed -i "s/REGIONNAME/$REGION/g" archeplay-ecomm-demo/product/docker-compose.yaml
+sudo sed -i "s/TABLENAME/$TABLENAME/g" archeplay-ecomm-demo/product/docker-compose.yaml
+sudo cp archeplay-ecomm-demo/product/product.service /etc/systemd/system/product.service
+sudo systemctl daemon-reload
+sudo systemctl enable product.service
+sudo systemctl start product.service
+sleep 120
+sudo docker ps -a
