0a1768853a1d592608a68c9211fd0c911971487b
Mounika <mounika@archeplay.com>
2020-11-23 15:17:36 +0000

    script for ubuntu

--- scriptamazonlinux.sh
+++ scriptamazonlinux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set +x
-sudo apt-get install docker jq -y
+sudo yum install docker jq -y
 sudo usermod -a -G docker ec2-user
 sudo systemctl  start docker
 sudo systemctl status docker
@@ -8,7 +8,7 @@ sudo systemctl enable docker
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-sudo apt-get install git -y
+sudo yum -y install git
 sudo git clone https://github.com/mohanraz81/archeplay-ecomm-demo.git
 REGION=`curl --silent http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region`
 sudo sed -i "s/REGIONNAME/$REGION/g" archeplay-ecomm-demo/product/docker-compose.yaml
