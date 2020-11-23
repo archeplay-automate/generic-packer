43fb00622e0a8ac7b65706687a9c454cb4992ac1
Mounika <mounika@archeplay.com>
2020-11-23 15:25:43 +0000

    script for ubuntu

--- script.sh
+++ script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set +x
-sudo apt-get install docker jq -y
+sudo apt-get install jq -y
+apt-get install docker-ce docker-ce-cli containerd.io -y
 sudo usermod -a -G docker ec2-user
 sudo systemctl  start docker
 sudo systemctl status docker
